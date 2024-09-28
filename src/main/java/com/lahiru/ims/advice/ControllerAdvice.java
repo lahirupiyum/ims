b8ec68d2b3e3805b50b082610bf8a733bf8b1aaf
thushara-abeykoon <thushara.abeykoon99@gmail.com>
2024-09-28 08:31:22 +0530

    Added ControllerAdvice and new Methods to handle error responses in ResponseEntityManager

--- src/main/java/com/lahiru/ims/advice/ControllerAdvice.java
+++ src/main/java/com/lahiru/ims/advice/ControllerAdvice.java
@@ -1,5 +1,44 @@
 package com.lahiru.ims.advice;
 
+import com.lahiru.ims.common.dto.StandardReponse;
+import com.lahiru.ims.exception.DataConflictException;
+import com.lahiru.ims.exception.NotFoundException;
+import com.lahiru.ims.utils.ResponseEntityManager;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
 public class ControllerAdvice {
-    
+    private static final Logger log = LoggerFactory.getLogger(ControllerAdvice.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardReponse<Object>> handleMethodArgumentNotValid(MethodArgumentNotValidException exception, WebRequest request) {
+        logError(exception, request);
+        return ResponseEntityManager.badRequest(exception.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<StandardReponse<Object>> handleNotFound(NotFoundException notFoundException, WebRequest webRequest) {
+        log.error(notFoundException.getMessage(), notFoundException);
+        logError(notFoundException, webRequest);
+        return ResponseEntityManager.notFound(notFoundException.getMessage());
+    }
+
+    @ExceptionHandler(DataConflictException.class)
+    public ResponseEntity<StandardReponse<Object>> handleConflict(DataConflictException dataConflictException, WebRequest webRequest) {
+        logError(dataConflictException, webRequest);
+        return ResponseEntityManager.conflict(dataConflictException.getMessage());
+    }
+
+    private void logError(Exception e, WebRequest request) {
+        log.error("Error occurred in {}", request.getContextPath());
+        log.error(e.getMessage());
+    }
 }
--- src/main/java/com/lahiru/ims/utils/ResponseEntityManager.java
+++ src/main/java/com/lahiru/ims/utils/ResponseEntityManager.java
@@ -1,5 +1,6 @@
 package com.lahiru.ims.utils;
 
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -9,6 +10,7 @@ import com.lahiru.ims.common.dto.StandardReponse;
 public class ResponseEntityManager {
 
     private static final int CREATED_RESPONSE_VALUE = HttpStatus.CREATED.value();
+    public static final int NOT_FOUND = HttpStatus.NOT_FOUND.value();
 
     public static <T> ResponseEntity<PaginationResponse<T>> page(PaginationResponse<T> page) {
         return ResponseEntity.ok().body(page);
@@ -23,5 +25,21 @@ public class ResponseEntityManager {
 
     public static <T> ResponseEntity<StandardReponse<T>> ok(T data) {
         return ResponseEntity.ok().body(new StandardReponse<>(data));
+    }
+
+    public static ResponseEntity<StandardReponse<Object>> error(int statusCode, String message) {
+        return ResponseEntity.status(statusCode).body(new StandardReponse<>(statusCode, message, null));
+    }
+
+    public static ResponseEntity<StandardReponse<Object>> notFound(String message) {
+        return error(NOT_FOUND, message);
+    }
+
+    public static ResponseEntity<StandardReponse<Object>> conflict(String message) {
+        return error(HttpStatus.CONFLICT.value(), message);
+    }
+
+    public static ResponseEntity<StandardReponse<Object>> badRequest(String message) {
+        return error(HttpStatus.BAD_REQUEST.value(), message);
     }
 }
