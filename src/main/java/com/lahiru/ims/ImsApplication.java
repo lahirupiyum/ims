625caabe8180835a217b13adfe28cd84fd39b6c0
Lahiru Piyamantha <lahirupiyumanthahemathilake@gmail.com>
2024-10-06 18:02:56 +0530

    Configured Cors Mappings

--- src/main/java/com/lahiru/ims/ImsApplication.java
+++ src/main/java/com/lahiru/ims/ImsApplication.java
@@ -2,6 +2,7 @@ package com.lahiru.ims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class ImsApplication {
