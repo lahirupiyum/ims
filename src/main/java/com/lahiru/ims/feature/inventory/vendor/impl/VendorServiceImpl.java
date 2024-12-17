ec303455a891a64495664ff400beec8b44eab437
gayan <gcdisanayaka1998@gmail.com>
2024-12-17 23:22:31 +0530

    new ModelMapper Configuration And Dependency Used in Vendor Service impl

--- src/main/java/com/lahiru/ims/feature/inventory/vendor/impl/VendorServiceImpl.java
+++ src/main/java/com/lahiru/ims/feature/inventory/vendor/impl/VendorServiceImpl.java
@@ -4,7 +4,6 @@ package com.lahiru.ims.feature.inventory.vendor.impl;
 import com.lahiru.ims.common.dto.PaginationResponse;
 import com.lahiru.ims.exception.DataConflictException;
 import com.lahiru.ims.exception.NotFoundException;
-
 import com.lahiru.ims.feature.inventory.vendor.Vendor;
 import com.lahiru.ims.feature.inventory.vendor.VendorMappers;
 import com.lahiru.ims.feature.inventory.vendor.VendorRepo;
@@ -13,6 +12,7 @@ import com.lahiru.ims.feature.inventory.vendor.dto.VendorRequestDto;
 import com.lahiru.ims.feature.inventory.vendor.dto.VendorResponseDto;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -32,18 +32,19 @@ public class VendorServiceImpl implements VendorService {
     private final VendorRepo vendorRepo;
     private  final VendorMappers vendorMappers;
     private final boolean activeStatus = true;
-
+    private final ModelMapper modelMapper;
     @Override
     public PaginationResponse<VendorResponseDto> findByPageWise(int page, int pageSize) throws Exception {
-//        Pageable pageable = PageRequest.of(page, pageSize);
-//        Page<Vendor> vendorPage = vendorRepo.findAllByIsActive(pageable, activeStatus);
-//        List<VendorResponseDto> vendorList = vendorPage.map(vendorMappers::toResponseDto).toList();
-//
-//        return new PaginationResponse<>(
-//                vendorList,
-//                (int) vendorPage.getTotalElements()
-//        );
-        return null;
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Vendor> vendorPage = vendorRepo.findAllByIsActive(pageable, activeStatus);
+        log.info("vendor: {}", vendorPage);
+        List<VendorResponseDto> vendorList = vendorPage.map(vendorMappers::toResponseDto).toList();
+        log.info("vendor: {}", vendorList);
+        return new PaginationResponse<>(
+                vendorList,
+                (int) vendorPage.getTotalElements()
+        );
+
     }
 
     @Override
@@ -57,28 +58,30 @@ public class VendorServiceImpl implements VendorService {
 
         if (isVendorExistsByEmail(requestDto.getEmail()))
             throw new DataConflictException(VENDOR_EMAIL);
-        log.info("vendor:{}",requestDto.getContactNo());
-        Vendor vendor = vendorMappers.toEntity(requestDto);
-        log.info("vendor:{}",vendor.getContactNo());
-        Vendor savedVendor = vendorRepo.save(vendor);
-
-        return vendorMappers.toResponseDto(savedVendor);
+//        log.info("vendor:{}",requestDto.getContactNo());
+//        Vendor vendor = vendorMappers.toEntity(requestDto);
+//        log.info("vendor:{}",vendor.getContactNo());
+//        Vendor savedVendor = vendorRepo.save(vendor);
+        Vendor vendor =modelMapper.map(requestDto,Vendor.class);
+        Vendor saveVendor = vendorRepo.save(vendor);
+        VendorResponseDto response =modelMapper.map(saveVendor,VendorResponseDto.class);
+        return response;
     }
 
     @Override
     public VendorResponseDto updateOne(int id, VendorRequestDto requestDto) throws Exception {
-//
-//        String updatedEmail = requestDto.getEmail();
-//
-//        Vendor vendor = vendorExistsById(id);
-//
-//        if (isVendorExistsByEmail(updatedEmail) && !vendor.getEmail().equals(updatedEmail))
-//            throw new DataConflictException(VENDOR_EMAIL);
-//
-//        Vendor vendorToBeUpdated = VendorMapper.toModel(requestDto);
-//        vendorToBeUpdated.setId(id);
-//
-//        Vendor updatedVendor = vendorRepo.save(vendorToBeUpdated);
+
+        String updatedEmail = requestDto.getEmail();
+
+        Vendor vendor = vendorExistsById(id);
+
+        if (isVendorExistsByEmail(updatedEmail) && !vendor.getEmail().equals(updatedEmail))
+            throw new DataConflictException(VENDOR_EMAIL);
+
+        Vendor vendorToBeUpdated = vendorMappers.toEntity(requestDto);
+        vendorToBeUpdated.setId(id);
+
+        Vendor updatedVendor = vendorRepo.save(vendorToBeUpdated);
 //
 //        return VendorMapper.toDto(updatedVendor);
         return null;
@@ -87,7 +90,7 @@ public class VendorServiceImpl implements VendorService {
     @Override
     public VendorResponseDto deleteOne(int id) throws Exception {
         Vendor vendor = vendorExistsById(id);
-        vendor.setIsActive(false);
+        vendor.setActive(false);
         vendorRepo.save(vendor);
 
 //        return VendorMapper.toDto(vendor);
