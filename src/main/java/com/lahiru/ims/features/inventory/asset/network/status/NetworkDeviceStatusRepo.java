62d310efa8e9be9399c0738f746ee192ed1059c3
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-12-18 09:03:09 +0530

    deleted unnecessary codes

--- src/main/java/com/lahiru/ims/features/inventory/asset/network/status/NetworkDeviceStatusRepo.java
+++
@@ -1,7 +0,0 @@
-package com.lahiru.ims.features.inventory.asset.network.status;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NetworkDeviceStatusRepo extends JpaRepository<NetworkDeviceStatus, Integer> {
-    
-}
