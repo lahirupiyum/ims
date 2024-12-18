6a2793b1f4f01afef74bcebb2ffa3fa7ebc98e2d
gayan <gcdisanayaka1998@gmail.com>
2024-12-18 22:32:09 +0530

    AssetResponseDto Created Attribute

--- src/main/java/com/lahiru/ims/common/dto/feature/AssetResponseDto.java
+++ src/main/java/com/lahiru/ims/common/dto/feature/AssetResponseDto.java
@@ -1,4 +1,25 @@
 package com.lahiru.ims.common.dto.feature;
 
+import com.lahiru.ims.feature.inventory.manufacturer.dto.ManufacturerRequestDto;
+import com.lahiru.ims.feature.inventory.model.dto.ModelDto;
+import com.lahiru.ims.feature.inventory.status.dto.StatusDto;
+import com.lahiru.ims.feature.inventory.type.dto.TypeDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AssetResponseDto {
+    private Integer id;
+    private String assetNumber;
+    private String serialNumber;
+    private Integer vendorId;
+    private Integer location;
+    private int quantity;
+    private ModelDto model;
+    private TypeDto type;
+    private StatusDto status;
+    private ManufacturerRequestDto manufacturer;
 }
