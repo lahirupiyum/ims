531cd76d22d915fa98462d68954ae726c340750f
gayan <gcdisanayaka1998@gmail.com>
2024-12-18 22:25:46 +0530

    implement And Test Complete  CRUD Of Location Controller

--- src/main/java/com/lahiru/ims/feature/inventory/location/impl/LocationServiceImpl.java
+++ src/main/java/com/lahiru/ims/feature/inventory/location/impl/LocationServiceImpl.java
@@ -2,49 +2,76 @@ package com.lahiru.ims.feature.inventory.location.impl;
 
 import com.lahiru.ims.common.dto.PaginationResponse;
 import com.lahiru.ims.exception.NotFoundException;
-
 import com.lahiru.ims.feature.inventory.location.Location;
-import com.lahiru.ims.feature.inventory.location.LocationMapper;
 import com.lahiru.ims.feature.inventory.location.LocationRepo;
 import com.lahiru.ims.feature.inventory.location.LocationService;
 import com.lahiru.ims.feature.inventory.location.dto.LocationRequestDto;
 import com.lahiru.ims.feature.inventory.location.dto.LocationResponseDto;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
-@Transactional
+//@Transactional
 @RequiredArgsConstructor
 public class LocationServiceImpl implements LocationService {
+    private final ModelMapper modelMapper;
+    private final LocationRepo repository;
+    public static final String LOCATION = "Location";
+
     @Override
     public PaginationResponse<LocationResponseDto> findByPageWise(int page, int pageSize) throws Exception {
-        return null;
+        Pageable pageable = PageRequest.of(page, pageSize);
+        Page<Location> locationPage = repository.findAll(pageable);
+        List<LocationResponseDto> locationList = modelMapper
+                .map(locationPage.stream().toList()
+                        , new TypeToken<List<LocationResponseDto>>() {
+                        }.getType());
+        return new PaginationResponse<>(
+                locationList,
+                (int) locationPage.getTotalElements()
+        );
     }
 
     @Override
     public List<LocationResponseDto> findAll() throws Exception {
-        return List.of();
+        List<Location> locationList = repository.findAll();
+        List<LocationResponseDto> responseLocation = modelMapper.map(locationList, new TypeToken<List<LocationResponseDto>>() {
+        }.getType());
+        return (!responseLocation.isEmpty()) ? responseLocation : Collections.emptyList();
     }
 
     @Override
     public LocationResponseDto createOne(LocationRequestDto locationRequestDto) throws Exception {
-        return null;
+        Location location = modelMapper.map(locationRequestDto, Location.class);
+        Location saveLocation = repository.save(location);
+        return modelMapper.map(saveLocation, LocationResponseDto.class);
     }
 
     @Override
     public LocationResponseDto updateOne(int id, LocationRequestDto locationRequestDto) throws Exception {
-        return null;
+        Location findLocation = repository.findById(id).orElseThrow(() -> new NotFoundException(LOCATION));
+        findLocation.setAddress(locationRequestDto.getAddress());
+        findLocation.setName(locationRequestDto.getName());
+        Location updatedLocation = repository.saveAndFlush(findLocation);
+        return modelMapper.map(updatedLocation, LocationResponseDto.class);
     }
 
     @Override
     public LocationResponseDto deleteOne(int id) throws Exception {
-        return null;
+        Location location = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException(LOCATION));
+        if (location != null) {
+            repository.deleteById(id);
+        }
+        return modelMapper.map(location, LocationResponseDto.class);
     }
 //
 //    public static final String BRANCH = "Branch";
