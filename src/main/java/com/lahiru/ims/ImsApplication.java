d9b830b7c1625a410c8222ff714a7048f85a9079
DMGCD <gcdisanayaka1998@gmail.com>
2024-12-20 11:33:47 +0530

    CommandLineRunner For Run time Application To Add Locations

--- src/main/java/com/lahiru/ims/ImsApplication.java
+++ src/main/java/com/lahiru/ims/ImsApplication.java
@@ -1,14 +1,33 @@
 package com.lahiru.ims;
 
+import com.lahiru.ims.feature.inventory.location.Location;
+import com.lahiru.ims.feature.inventory.location.LocationRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @SpringBootApplication
 public class ImsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ImsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(LocationRepo locationRepo) {
+        return args -> {
+            List<Location> locations = new ArrayList<>(List.of(new Location("Taj-Hotel", "colombo-02"), new Location("Orin-Hotel", "colombo-07")));
+            // Add roles
+            if (locationRepo.count() == 0) {
+                locationRepo.saveAll(locations);
+            }
+
+        };
+    }
 
 }
