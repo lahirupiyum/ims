5cebad3dd4a01e2959930991175d628a85af9e2f
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2024-12-27 12:00:23 +0530

    added missing id field for customer response and customer router response

--- src/main/java/com/lahiru/ims/feature/customer/customer/dto/CustomerResponseDto.java
+++ src/main/java/com/lahiru/ims/feature/customer/customer/dto/CustomerResponseDto.java
@@ -1,4 +1,12 @@
 package com.lahiru.ims.feature.customer.customer.dto;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
 public class CustomerResponseDto extends CustomerDto{
+    private Integer id;
 }
