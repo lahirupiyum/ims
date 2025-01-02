c83b4696bfa5314dab32e71ca79ee7faa5383d8f
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2025-01-02 19:19:59 +0530

    updated asset dto

--- src/main/java/com/lahiru/ims/common/dto/feature/AssetRequestDto.java
+++ src/main/java/com/lahiru/ims/common/dto/feature/AssetRequestDto.java
@@ -1,24 +1,17 @@
 package com.lahiru.ims.common.dto.feature;
 
-import com.lahiru.ims.feature.inventory.manufacturer.dto.ManufacturerDto;
-import com.lahiru.ims.feature.inventory.model.dto.ModelDto;
-import com.lahiru.ims.feature.inventory.type.dto.TypeDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class AssetRequestDto {
-    private String assetNumber;
-    private String serialNumber;
-    private int quantity;
-    private int statusId;
+public class AssetRequestDto extends AssetDto {
+    private Integer statusId;
     private Integer vendorId;
     private Integer locationId;
-    private ManufacturerDto manufacturer;
-    private TypeDto type;
-    private ModelDto model;
 
 }
