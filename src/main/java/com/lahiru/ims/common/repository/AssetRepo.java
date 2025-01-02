fd3e58186b305280f2d7351d016048d1341b7acd
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2025-01-02 18:54:15 +0530

    Added missing no repository bean annotations for custom repos

--- src/main/java/com/lahiru/ims/common/repository/AssetRepo.java
+++ src/main/java/com/lahiru/ims/common/repository/AssetRepo.java
@@ -2,9 +2,11 @@ package com.lahiru.ims.common.repository;
 
 import com.lahiru.ims.common.model.AssetAudit;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface AssetRepo <Model extends AssetAudit> extends StatusAwareRepo<Model> {
     @Query("SELECT e FROM #{#entityName} e WHERE e.serialNumber LIKE %:serialNumber%")
     List<Model> findBySerialNumber(String serialNumber);
--- src/main/java/com/lahiru/ims/common/repository/BasicInfoRepo.java
+++ src/main/java/com/lahiru/ims/common/repository/BasicInfoRepo.java
@@ -3,9 +3,11 @@ package com.lahiru.ims.common.repository;
 import com.lahiru.ims.common.enums.AssetType;
 import com.lahiru.ims.common.model.BasicInfoAudit;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface BasicInfoRepo <Model extends BasicInfoAudit> extends IDNameRepo<Model>{
     @Query("SELECT e FROM #{#entityName} e WHERE e.assetType = :assetType")
     List<Model> findAllByAssetType(AssetType assetType);
